0ac1f9aa7b45c057e26452b1d729f56cf1afba9a
Atma Manomaya <140921611+Atmandev84@users.noreply.github.com>
2024-10-28 23:49:14 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		int num1 = 1347;
+		int num2 = 2234;
+		System.out.println(num1-num2);
+
+	}
+
+}
